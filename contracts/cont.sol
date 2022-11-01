1eccf609d2f87652bf0df52c64921b5b0d12e750
hex <waverew@gmail.com>
2022-10-31 22:48:16 -0400

    updated betting contract

--- contracts/cont.sol
+++ contracts/cont.sol
@@ -2,16 +2,20 @@
 
 pragma solidity ^0.8.0;
 contract beting {
-    mapping (address => uint) public bets;
+    mapping (address => uint) betAmount; 
+    mapping(address => bool) userBetted;
+    mapping(address => bool) votes; 
+    address[] public participants; // people who are betting
+    string[] public teams; // teams to bet on
     address private owner;
     uint private amount;
     event OwnerSet(address indexed oldOwner, address indexed newOwner);
     modifier nonZeroValue(){
         if (!(msg.value > 0)) revert();
         _;
-    }
+    } // prevents zero value calls
     modifier isOwner() {
-        require (msg.sender == owner, "you are not the owner");
+        require (msg.sender == owner, "you are not the owner"); 
         _;
     }
     event setOwneship(address indexed oldOwner, address indexed newOwner);
@@ -30,9 +34,11 @@ contract beting {
     fallback() external payable nonZeroValue{
         amount += msg.value/ 1 ether;
     }
-    function makeBet(uint _amount) public{
-        bets[msg.sender] += _amount;
-        amount+=_amount;
+    function makeBet(uint _amount) public payable{
+        userBetted[msg.sender] = true;
+        amount += _amount;
+
+        
     }
 
     
