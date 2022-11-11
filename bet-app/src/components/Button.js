9cef965fe352dfc9a9b915f55d6ac4c9c4e630ae
hex <waverew@gmail.com>
2022-11-11 17:07:22 -0500

    added betting func

--- bet-app/src/components/Button.js
+++ bet-app/src/components/Button.js
@@ -1,30 +1,36 @@
 import { useState } from "react";
+import "./Button.css";
 const Button = () =>{
-const [s, setS] = useState(null);
-var state = {
-    team:null,
-    amount:null
-}
-const handleSubmit = (event)=>{
-    setS(event);
-    console.log(state);
-    console.log(s);
-
-    
+const [team, setTeam] = useState("");
+const [amount, setAmount] = useState("");
+const state = {
+    team:"",
+    amount:""
 }
     return(
-        <form >
-            <label>
-                Enter team name
-                <input type="text" value={state.team} onInput={()=>{handleSubmit()}}/>
-            </label>
-            <label>
-                Enter bet amount
-                <input type="text" value={state.amount} onInput={()=>{handleSubmit()}}/>
-            </label>
-            <input type="submit"/>
-
-        </form>
+        <div className="butt">
+            <div>
+                <div className="cont">
+                    <label>
+                    Enter team name:
+                        <input type="text" value={team} onChange={(e)=>{setTeam(e.target.value)}} className="cont"/>
+                    </label>
+                    <br/>
+                    <label>
+                    Enter bet amount:
+                        <input type="text" value={amount} onChange={(e)=>{setAmount(e.target.value)}} className="cont"/>
+                    </label>
+                </div>
+            
+                <p>{team}</p>
+                <div className="b">
+                    <button onClick={()=>{state.team=team; state.amount=amount; console.log(state)}}>
+                    Submit
+                    </button>
+                </div>
+            </div>
+        </div>
+        
     )
 }
 export default Button;
